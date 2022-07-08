63ac3c1bd02eabb6527ba9a4e20d5ea2981552f8
martin jerez <martincho111986@gmail.com>
2022-07-07 21:09:23 -0300

    repo para fundamentos

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Elegir zapatos de fiesta
+// 1. INICIO
+// 2. Entrar a la tienda y buscar la sección de zapatos de caballero.
+// 3. Tomar un par de zapatos. DATOS
+// ¿ 4.Son zapatos de fiesta? - PROCESO
+//  a- SI: (ir al paso 5) – NO: (volver al paso 3)
+// 5. ¿Hay de la talla adecuada? - PROCESO
+//  a- SI: (ir al paso 6) – NO: (volver al paso 3)
+// 6. ¿El precio es pagable? - PROCESO
+// a- SI: (ir al paso 7) – NO: (volver al paso 3)
+// 7. Comprar el par de zapatos elegido. - RESULTADO
+// 8. FIN
