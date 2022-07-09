81af93409c232187485d0c1fbaa3d643fcdb1238
darien-yoel <yoeldarien@gmail.com>
2022-07-09 20:25:18 -0300

    Algoritmo para compra de notebook

---
+++ js/darien.js
@@ -0,0 +1,37 @@
+//* Problema a resolver
+
+//? COMPRAR UNA NOTEBOOK COMPRAR
+
+//* Analisis
+
+//? Se  busca comprar una NOTEBOOK con las siguientes especificaciones y que este dentro del presupuesto
+
+//TODO - Procesador i5 o Ryzen5 - Ram 16 gigas - Almacenamiento ssd  - Pantalla FHD 
+
+//! Presupuesto $130000
+
+//* Elegir Notebook
+
+//* 1- INICIO
+//* 2- Donde comprar? PROCESO
+//* A. COMPRAR POR INTERNET.(IR AL PASO 3)
+//* B. IR A UNA TIENDA. (NO SALGAS ALTA FIACA SEGUIR AGREGANDO PASOS COMPRA POR INTERNET)
+//* 3- Sentarse en la pc e ingresar a la pagina de la tienda.
+//* 4- Elegir tipo de procesador. PROCESO
+//* A. INTEL.
+//* B. AMD.
+//* 5- ¿Tiene 16 gigas de ram? PROCESO
+//* A. SI. (IR AL PASO 6)
+//* B. NO (VOLVER PASO 4)
+//* 6- ¿Tiene ssd? PROCESO
+//* A. SI (IR AL PASO 7)
+//* B. NO (IR AL PASO 5)
+//* 7- ¿Tiene pantalla FHD? PROCESO
+//* A. SI (IR AL PASO 8)
+//* B. NO (IR AL PASO 6)
+//* 8- ¿Esta dentro del presupuesto? PROCESO
+//* A. SI (IR AL PASO 9)
+//* B. NO (Y BUE MEJOR SEGUI AHORRANDO)
+//* 9- Comprar la notebook para jugar minecraft. RESULTADO
+//* FIN.
+
