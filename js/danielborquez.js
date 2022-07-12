9a4a92bc211433a749037bab90f9e3ace53cc67b
NielzBrz <danielbrz1999@gmail.com>
2022-07-12 12:32:39 -0300

    Preparar te

---
+++ js/danielborquez.js
@@ -0,0 +1,62 @@
+// *Problema a resolver
+// Preparar una taza de té.
+
+// *Analisis
+// Quiero tomar té.
+
+// 1. INICIO
+// 2. ¿Tengo un saquito de té?  - PROCESO
+// a. - Si: (Ir al paso 3) 
+// b. - No: (Ir al paso 7)
+// 3. ¿Tengo agua?  - PROCESO
+// a. - Si: (Ir al paso 4) 
+// b. - No: (Ir al paso 7)
+// 4. ¿Tengo una paba?  - PROCESO
+// a. - Si: (Ir al paso 5) 
+// b. - No: (Ir al paso 7)
+// 5. ¿Tengo una taza?  - PROCESO 
+// a. - Si: (Ir al paso 6) 
+// b. - No: (Ir al paso 7)
+// 6. ¿Tengo azucar?  - PROCESO
+// a. - Si: (Ir al paso 11) 
+// b. - No: (Ir al paso 7)
+
+// 7. Salir de mi casa.
+// 8. Ir a un negocio.
+// 9. Comprar lo faltante.
+// 10. Volver a mi casa. (Ir al paso 2)
+
+// 11. LLenar la paba con la cantidad de agua dispuesta a tomar
+// 12. ¿La paba es eléctrica?  - PROCESO
+// a. - Si: (Ir al paso 13) 
+// b. - No: (Ir al paso 14)
+
+// 13. Enchufarla a la corriente eléctrica (Ir al paso 15)
+// 14. Encender la hornalla.
+
+// 15. Esperar.
+// 16. ¿El agua está hirviendo?  - PROCESO
+// a. - Si: (Ir al paso 17) 
+// b. - No: (Ir al paso 15)
+
+// 17. ¿La paba es eléctrica?  - PROCESO
+// a. - Si: (Ir al paso 18) 
+// b. - No: (Ir al paso 19)
+
+// 18. Desenchufar. (Ir al paso 20)
+// 19. Apagar la hornalla.
+
+// 20. Colocar un saquito de té dentro de la taza.
+// 21. Volcar el agua caliente dentro de la taza.
+// 22. Mover el saquito dentro del agua 10 segundos.
+// 23. Quitar el saquito.
+// 24. Colocar 15gm de azucar en la taza.
+
+// 25. Esperar.
+
+// 26. ¿El té está frío?  - PROCESO
+// a. - Si: (Ir al paso 29) 
+// b. - No: (Ir al paso 27)
+
+// 27. Tomár.
+// 28. FIN
