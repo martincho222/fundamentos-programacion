5097cbcb2ed84b4227cc399257b13b9953d67ebe
martin jerez <martincho111986@gmail.com>
2022-07-19 21:54:16 -0300

    clase de estructuras condicionales

---
+++ js/03-number.js
@@ -0,0 +1,13 @@
+let number1 = 10.2;
+
+// console.log(Number.isFinite(number1)); // me indica si el numero es infinito o no
+// console.log(Number.isNaN(number1));
+
+// console.log(Number.isInteger(number1)); // verifica si el numero es entero
+// console.log(Number.parseFloat(number1));
+// console.log(Math.random(100) * 1000); // devuelve un numero random entre 0 y 1
+
+// console.log(Math.floor(number1)) // devuelve el valor maximo entero menor o igual
+// console.log(Math.ceil(number1))// devuelve el valor maximo entero mayor o igual
+// console.log(Math.round(number1)); //este metodo retorna el valor entero rendondeado 
+// console.log(Math.pow(2, 5)); // retorna la base elevada a la N potencia.
---
+++ js/05-switch.js
@@ -0,0 +1,54 @@
+// switch (condicion) {
+//     case :
+//     break;
+
+//     default:
+//         break;
+// }
+
+// const numero = 6;
+
+// switch (numero) {
+//   case 1:
+//     console.error("uno");
+//     break;
+//   case 2:
+//     console.log("dos");
+//     break;
+//   case 3:
+//     console.log("tres");
+//     break;
+//   case 4:
+//     console.log("cuatro");
+//     break;
+//   case 5:
+//     console.log("cinco");
+//     break;
+//   default:
+//     console.log("no es un numero valido");
+//     break;
+// }
+
+// const logType = prompt("ingrese un valor de log");
+
+// switch (logType) {
+//     case "error":
+//         console.error("error");
+//         break;
+//     case "warn":
+//         console.warn("warn");
+//         break;
+//     case "info":
+//         console.info("info");
+//         break;
+//     case "log":
+//         console.log("log");
+//         break;
+//     case "write":
+//         document.write("write");
+//         break;
+//     default:
+//         console.error("no es un tipo de log valido");
+//         break;
+// }
+
