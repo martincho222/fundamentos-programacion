2dced769426a66c68b09e15f0f43ec0bb622609f
sebazelarayan12 <sebazelarayan12@gmail.com>
2022-09-03 20:04:04 -0300

    agregué hasta punto de libros

---
+++ tareas js/js/arraysFunciones.js
@@ -0,0 +1,66 @@
+// 1-
+
+// let ciudades = [];
+// let ciudad = prompt("Ingrese una ciudad");
+
+// while (ciudad !== null && ciudad !== "0") {
+//   if (ciudad !== "") {
+//     ciudades.push(ciudad);
+//   }
+//   ciudad = prompt("Ingrese una ciudad");
+// }
+// console.log(ciudades);
+
+// console.log(`Cantidad de elementos en el arreglo: ${ciudades.length}`);
+
+// document.write(`1- ${ciudades[0]}<br>`);
+// document.write(`3- ${ciudades[2]}<br>`);
+// document.write(`ultima- ${ciudades[ciudades.length - 1]}<br>`);
+// ciudades.push("París");
+
+// document.write(`${ciudades[1]} ocupa la segunda posicion en el arreglo<br>`);
+
+// ciudades.splice(1, "Barcelona");
+
+
+// 2-
+
+// let a = prompt("ingrese longitud lado 1");
+// let b = prompt("ingrese longitud lado 2");
+
+// let perimetro = (a+b)*2;
+
+// alert(`El perimetro es ${perimetro}`); duda
+
+// 3-
+
+// let numero;
+// numero = prompt("Elige un numero");
+
+// if (numero % 2 == 0){
+//         document.write("es par");
+//     }
+//     else{
+//         document.write("es impar");
+//     }
+
+// 4-
+
+// function getRandomIntInclusive(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+//   }
+
+// sumaDados = [];
+
+// function lanzarDados(){
+//     for(let i=0; i<50 ; i++){
+//     let dado1 = getRandomIntInclusive(1, 6);
+//     let dado2 = getRandomIntInclusive(1, 6);
+//     let suma = dado1 + dado2;
+//     sumaDados.push(suma);
+//     }
+//     document.write(sumaDados);
+// }
+
